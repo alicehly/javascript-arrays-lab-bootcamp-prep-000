98676e455da51b5a3c05aa92b08c337606109414
Alice Ly Ms <alicehly@gmail.com>
2019-05-10 16:55:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -30,10 +30,7 @@ function prependKitten(name) {
 }
 
 function removeLastKitten() {
-  kittens.slice(0, kittens.length-1);
+  kittens.slice(0, kittens.length - 1);
   return kittens;
 }
 
-function removeFirstKitten() {
-  kittens.slice(0, kittens.length+1);
-}
